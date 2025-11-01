1bce1716c54a5bb9d4c9b9b1ba1fc170ad309c24
Ana María <anicu2314@gmail.com>
2025-11-01 09:34:56 -0500

    Ejercicio completo: Camino mínimo

--- src/main/java/algoritmos/Dinamica/CaminoMinimo.java
+++ src/main/java/algoritmos/Dinamica/CaminoMinimo.java
@@ -1,4 +1,40 @@
 package algoritmos.Dinamica;
 
+import java.util.Arrays;
+
+import static java.util.Collections.min;
+
 public class CaminoMinimo {
+
+    public static void main(String[] args) {
+        int [][] matriz= {{1,3,1}, {1,5,1}, {4,2,1}};
+        System.out.println("Matriz inicial");
+        System.out.println(Arrays.deepToString(matriz));
+        System.out.println("Matriz Resultante");
+        System.out.println(Arrays.deepToString(caminoMinimo2(matriz)));
+    }
+
+    public static int[][] caminoMinimo2(int [][] matriz){
+        int [][] dp= new int[matriz.length][matriz[0].length];
+        dp[0][0] = matriz[0][0];
+
+        for(int i = 1; i<matriz[0].length; i++){
+            dp[0][i] = matriz[0][i] + dp[0][i-1];
+        }
+
+        for(int i = 1; i<matriz.length; i++){
+            dp[i][0] = matriz[i][0] + dp[i-1][0];
+        }
+
+        for(int i = 1; i < matriz.length; i++){
+            for(int j = 1; j < matriz[0].length; j++){
+
+                int min = Math.min( dp[i-1][j], dp[i][j-1] );
+                dp[i][j] = matriz[i][j] + min;
+
+            }
+        }
+        return dp;
+    }
+
 }
--- src/main/java/algoritmos/Dinamica/MochilaEntera.java
+++ src/main/java/algoritmos/Dinamica/MochilaEntera.java
@@ -1,10 +1,61 @@
 package algoritmos.Dinamica;
 
+import java.util.Arrays;
+
 public class MochilaEntera {
 
     public static void main(String[] args) {
 
     }
 
+    public static int mochilaEntera(int [] valores, int [] pesos, int maxValue){
+        Objeto [][] dp = new Objeto[valores.length+1][maxValue+1];
+
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i], new Objeto(0, ""));
+        }
+
+        for(int i=1; i<valores.length; i++){
+            for(int j=1; j<maxValue+1; j++){
+                if( j - pesos[i] > 0 ){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    int max = Math.max(dp[i-1][j].getBeneficio(), valores[i] + dp[i-1][j - pesos[i]].beneficio);
+                }
+            }
+
+        }
+
+
+
+        return 0;
+    }
+
+    static class Objeto{
+
+        int beneficio;
+        String camino;
+
+        public Objeto(int beneficio, String camino){
+            this.beneficio =beneficio;
+            this.camino=camino;
+        }
+
+        public int getBeneficio() {
+            return beneficio;
+        }
+
+        public String getCamino() {
+            return camino;
+        }
+
+        public void setBeneficio(int beneficio) {
+            this.beneficio = beneficio;
+        }
+
+        public void setCamino(String camino) {
+            this.camino = camino;
+        }
+    }
 
 }
