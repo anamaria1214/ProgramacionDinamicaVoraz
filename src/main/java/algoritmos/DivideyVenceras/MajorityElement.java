38d26edeea7673cf4f8a2541fa2b9a5e2dcae818
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2025-11-03 14:54:28 -0500

    Solucion divide y venceras
    todo

--- src/main/java/algoritmos/DivideyVenceras/MajorityElement.java
+++ src/main/java/algoritmos/DivideyVenceras/MajorityElement.java
@@ -1,4 +1,62 @@
 package algoritmos.DivideyVenceras;
 
+import java.util.Scanner;
+
 public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+        return majorityRec(nums, 0, nums.length - 1);
+    }
+
+    private static int majorityRec(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = (left + right) / 2;
+        int leftMajor = majorityRec(nums, left, mid);
+        int rightMajor = majorityRec(nums, mid + 1, right);
+
+        if (leftMajor == rightMajor) return leftMajor;
+
+        int leftCount = count(nums, leftMajor, left, right);
+        int rightCount = count(nums, rightMajor, left, right);
+
+        return leftCount > rightCount ? leftMajor : rightMajor;
+    }
+
+    private static int count(int[] nums, int num, int left, int right) {
+        int cnt = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == num) cnt++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== MAYORITY ELEMENT ===");
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Ingrese los elementos del arreglo:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int candidate = majorityElement(nums);
+
+        // Verificar si realmente aparece más de la mitad
+        int count = 0;
+        for (int num : nums) if (num == candidate) count++;
+
+        if (count > n / 2)
+            System.out.println("Elemento mayoritario: " + candidate);
+        else
+            System.out.println("No hay mayoría. Retorna: -1");
+    }
 }
+
+
--- src/main/java/algoritmos/DivideyVenceras/SmallerElementsCount.java
+++ src/main/java/algoritmos/DivideyVenceras/SmallerElementsCount.java
@@ -1,4 +1,77 @@
 package algoritmos.DivideyVenceras;
+import java.util.*;
 
 public class SmallerElementsCount {
+
+    public static List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        Integer[] counts = new Integer[n];
+        Arrays.fill(counts, 0);
+
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums[i];
+            pairs[i][1] = i;
+        }
+
+        mergeSort(pairs, 0, n - 1, counts);
+        return Arrays.asList(counts);
+    }
+
+    private static void mergeSort(int[][] pairs, int left, int right, Integer[] counts) {
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+        mergeSort(pairs, left, mid, counts);
+        mergeSort(pairs, mid + 1, right, counts);
+        merge(pairs, left, mid, right, counts);
+    }
+
+    private static void merge(int[][] pairs, int left, int mid, int right, Integer[] counts) {
+        List<int[]> temp = new ArrayList<>();
+        int i = left, j = mid + 1;
+        int rightCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (pairs[i][0] <= pairs[j][0]) {
+                counts[pairs[i][1]] += rightCount;
+                temp.add(pairs[i++]);
+            } else {
+                rightCount++;
+                temp.add(pairs[j++]);
+            }
+        }
+
+        while (i <= mid) {
+            counts[pairs[i][1]] += rightCount;
+            temp.add(pairs[i++]);
+        }
+
+        while (j <= right) {
+            temp.add(pairs[j++]);
+        }
+
+        for (int k = left; k <= right; k++) {
+            pairs[k] = temp.get(k - left);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== SMALLER ELEMENTS COUNT ===");
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Ingrese los elementos del arreglo:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        List<Integer> result = countSmaller(nums);
+        System.out.println("Resultado: " + result);
+    }
 }
+
+
